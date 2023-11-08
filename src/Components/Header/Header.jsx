99dd94f8af0389822326aa88d81e2a02c92359ce
Kamrul Hasan <kamrul444216@gmail.com>
2023-11-09 04:03:25 +0600

    header section done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,74 @@
-
+import { Link, useLocation } from "react-router-dom";
+import { FaBars, FaUser } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+import { useState } from "react";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const nav = [
+    { id: 1, name: "Home", url: "/" },
+    { id: 2, name: "Destination", url: "/destination" },
+    { id: 3, name: "About", url: "/about" },
+    { id: 4, name: "Contact", url: "/contact" },
+  ];
 
-export default Header
+  const [open, setOpen] = useState(false);
+  const handleLinkClick = () => {
+    setOpen(false);
+  };
+
+  const { pathname } = useLocation();
+
+  return (
+    <header className="max-w-screen-xl mx-auto flex items-center justify-between relative py-2">
+      {/* Logo */}
+      <div className="pl-5">
+        <img className="max-w-[150px]" src="/logo.png" alt="" />
+      </div>
+
+      {/* Navbar */}
+      <ul
+        className={`flex gap-5 ${
+          open
+            ? "flex-col absolute top-16 justify-center items-center gap-10 bg-black text-white w-full h-[calc(100vh-64px)]"
+            : "hidden md:flex"
+        }`}
+      >
+        {nav.map((item) => (
+          <Link
+            key={item.id}
+            to={item.url}
+            onClick={handleLinkClick}
+            className={`${
+              pathname === item.url
+                ? "text-[#FF5522] border-b-2 border-[#FF5522] font-semibold"
+                : "font-semibold text-[#6C7171] hover:text-[#FF5522] transition-all"
+            }`}
+          >
+            <li>{item.name}</li>
+          </Link>
+        ))}
+        <Link onClick={() => setOpen(!open)} to={"/login"}>
+          <button className="md:hidden flex items-center justify-center gap-1 font-semibold text-[#6C7171]">
+            <FaUser />
+            Login/Signup
+          </button>
+        </Link>
+      </ul>
+
+      {/* Login - Register */}
+      <div className="pr-5">
+        <Link onClick={() => setOpen(!open)} to={"/login"}>
+          <button className="hidden md:flex items-center justify-center gap-1 font-semibold text-[#6C7171]">
+            <FaUser />
+            Login/Signup
+          </button>
+        </Link>
+        <button className="md:hidden" onClick={() => setOpen(!open)}>
+          {open ? <FaXmark /> : <FaBars />}
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/Routers/Route.jsx
+++ src/Routers/Route.jsx
@@ -3,25 +3,40 @@ import Main from "../Layout/Main";
 import App from "../App";
 import SignUp from "../Pages/SIgnUP/SignUp";
 import SignIn from "../Pages/SIgnIn/SignIn";
+import Destination from "../Pages/Destination/Destination";
+import About from "../Pages/About/About";
+import Contact from "../Pages/Contact/Contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main/>,
+    element: <Main />,
     children: [
-        {
-            path: '/',
-            element: <App/>
-        },
-        {
-            path: '/sign-up',
-            element: <SignUp/>
-        },
-        {
-            path: '/login',
-            element: <SignIn/>
-        },
-    ]
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <SignIn />,
+      },
+      {
+        path: "/destination",
+        element: <Destination />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
