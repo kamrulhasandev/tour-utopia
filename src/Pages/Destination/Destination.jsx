a78817a301833d42f530383f8b725f83aaee7a8e
Kamrul Hasan <kamrul444216@gmail.com>
2023-11-12 03:32:08 +0600

    destination page

--- src/Pages/Destination/Destination.jsx
+++ src/Pages/Destination/Destination.jsx
@@ -1,7 +1,74 @@
-const Destination = () => {
-  return (
-    <div>Destination</div>
-  )
-}
+import { useEffect, useState } from "react";
+import { FaClock, FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-export default Destination
+const Destination = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/data.json");
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div className="py-20">
+      <div className="max-w-screen-xl mx-auto px-5">
+        <div className="grid grid-cols-1 md:grid-cols-3 justify-center items-center gap-5 py-10">
+          {data.map((item, index) => (
+            <div key={index} className="bg-white shadow-lg rounded-2xl">
+              <Link to={`/package/${item.id}`}>
+                <div className="image-container rounded-t-2xl">
+                  <img className="rounded-t-2xl" src={item.image} alt="" />
+                  <div className="overlay"></div>
+                </div>
+                <div className="p-5">
+                  <div className="flex flex-col gap-3">
+                    <p className="text-[#FF5522] font-bold text-lg">
+                      {item.location}
+                    </p>
+                    <h6 className="text-xl font-semibold text-[#6C7171]">
+                      {item.name}
+                    </h6>
+                    <div className="flex justify-between items-center">
+                      <p className="flex  items-center gap-1">
+                        <FaClock className="text-[#FF5522]" size={15} />
+                        <span className="text-[#6C7171] font-bold uppercase">
+                          {item.duration}
+                        </span>
+                      </p>
+                      <p className="text-[#FF5522] font-bold">
+                        {item.category}
+                      </p>
+                    </div>
+                  </div>
+                  <hr className="my-3" />
+                  <div className="flex justify-between">
+                    <p className="flex items-center gap-1">
+                      <FaStar />
+                      <span className="text-[#6C7171]">4.9</span>
+                    </p>
+                    <p className="text-[#6C7171]">
+                      Start From:{" "}
+                      <span className="text-[#FF5522] font-bold">
+                        ${item.price}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Destination;
--- src/Pages/PackageDetails/PackageDetails.jsx
+++ src/Pages/PackageDetails/PackageDetails.jsx
@@ -1,10 +1,14 @@
 import { useContext, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
 import { FaClock, FaStar, FaLongArrowAltRight } from "react-icons/fa";
 
 const PackageDetails = () => {
   const [data, setData] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [showNotLoggedInPopup, setShowNotLoggedInPopup] = useState(false);
+
+  const navigate = useNavigate();
   const { id } = useParams();
   const { user } = useContext(AuthContext);
 
@@ -23,8 +27,24 @@ const PackageDetails = () => {
   }, []);
 
   const matchingResults = data.find((item) => item.id == id);
-  const popularTour = data.filter((item) => item.location === matchingResults?.location)
+  const popularTour = data.filter(
+    (item) => item.location === matchingResults?.location
+  );
   console.log(popularTour);
+
+  const handleBookNow = () => {
+    if (user?.email) {
+      // User is logged in, show booking success popup
+      setShowPopup(true);
+    } else {
+      // User is not logged in, show not logged in popup
+      setShowNotLoggedInPopup(true);
+    }
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
 
   return (
     <div className="">
@@ -140,7 +160,10 @@ const PackageDetails = () => {
                   />
                 </div>
               </div>
-              <button className="bg-[#FF5522] font-bold py-3 text-white w-full">
+              <button
+                onClick={handleBookNow}
+                className="bg-[#FF5522] font-bold py-3 text-white w-full"
+              >
                 Book Now
               </button>
             </div>
@@ -152,7 +175,7 @@ const PackageDetails = () => {
           <ul className="flex flex-col gap-2 py-2">
             {matchingResults?.highlights?.map((item, index) => (
               <li key={index} className="flex items-center gap-3">
-                <FaLongArrowAltRight className="text-[#FF5522]"/>
+                <FaLongArrowAltRight className="text-[#FF5522]" />
                 {item}
               </li>
             ))}
@@ -163,48 +186,118 @@ const PackageDetails = () => {
           <ul className="flex flex-col gap-2 py-2">
             {matchingResults?.recommendedGear?.map((item, index) => (
               <li key={index} className="flex items-center gap-3">
-                <FaLongArrowAltRight className="text-[#FF5522]"/>
+                <FaLongArrowAltRight className="text-[#FF5522]" />
                 {item}
               </li>
             ))}
           </ul>
         </div>
 
-
         {/* Popular tour  */}
 
         <div className="pt-20 pb-5">
-            <h3 className="text-4xl font-bold">Popular Tour in {popularTour[0]?.location}</h3>
+          <h3 className="text-4xl font-bold">
+            Popular Tour in {popularTour[0]?.location}
+          </h3>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 justify-center items-center gap-5 py-10">
           {popularTour.map((item, index) => (
             <div key={index} className="bg-white shadow-lg rounded-2xl">
               <Link to={`/package/${item.id}`}>
-              <div className="image-container rounded-t-2xl">
-                <img className="rounded-t-2xl" src={item.image} alt="" />
-                <div className="overlay"></div>
-              </div>
-              <div className="p-5">
-                <div className="flex flex-col gap-3">
-                  <p className="text-[#FF5522] font-bold text-lg">{item.location}</p>
-                  <h6 className="text-xl font-semibold text-[#6C7171]">{item.name}</h6>
-                  <div className="flex justify-between items-center">
-                    <p className="flex  items-center gap-1"><FaClock className="text-[#FF5522]" size={15} /><span className="text-[#6C7171] font-bold uppercase">{item.duration}</span></p>
-                    <p className="text-[#FF5522] font-bold">{item.category}</p>
+                <div className="image-container rounded-t-2xl">
+                  <img className="rounded-t-2xl" src={item.image} alt="" />
+                  <div className="overlay"></div>
+                </div>
+                <div className="p-5">
+                  <div className="flex flex-col gap-3">
+                    <p className="text-[#FF5522] font-bold text-lg">
+                      {item.location}
+                    </p>
+                    <h6 className="text-xl font-semibold text-[#6C7171]">
+                      {item.name}
+                    </h6>
+                    <div className="flex justify-between items-center">
+                      <p className="flex  items-center gap-1">
+                        <FaClock className="text-[#FF5522]" size={15} />
+                        <span className="text-[#6C7171] font-bold uppercase">
+                          {item.duration}
+                        </span>
+                      </p>
+                      <p className="text-[#FF5522] font-bold">
+                        {item.category}
+                      </p>
+                    </div>
+                  </div>
+                  <hr className="my-3" />
+                  <div className="flex justify-between">
+                    <p className="flex items-center gap-1">
+                      <FaStar />
+                      <span className="text-[#6C7171]">4.9</span>
+                    </p>
+                    <p className="text-[#6C7171]">
+                      Start From:{" "}
+                      <span className="text-[#FF5522] font-bold">
+                        ${item.price}
+                      </span>
+                    </p>
                   </div>
                 </div>
-                <hr className="my-3" />
-                <div className="flex justify-between">
-                  <p className="flex items-center gap-1"><FaStar /><span className="text-[#6C7171]">4.9</span></p>
-                  <p className="text-[#6C7171]">Start From: <span className="text-[#FF5522] font-bold">${item.price}</span></p>
-                </div>
-              </div>
               </Link>
             </div>
           ))}
         </div>
 
+        {/* Popup for booking success*/}
+        {showPopup && (
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
+            <div className="bg-white p-8 rounded-lg">
+              <p className="text-2xl font-bold text-[#6C7171] mb-4">
+                Booking Successful!
+              </p>
+              <p className="text-lg text-[#6C7171]">
+                Thank you for booking with us.
+              </p>
+              <button
+                onClick={closePopup}
+                className="mt-4 bg-[#FF5522] text-white font-bold py-2 px-4 rounded"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        )}
+        {/* Popup for not logged in */}
+
+        {showNotLoggedInPopup && (
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
+            <div className="bg-white p-8 rounded-lg">
+              <p className="text-2xl font-bold text-[#6C7171] mb-4">
+                You are not logged in!
+              </p>
+              <p className="text-lg text-[#6C7171]">
+                Please login to proceed with the booking.
+              </p>
+              <div className="flex justify-between">
+              <button
+                onClick={() => {
+                  setShowNotLoggedInPopup(false);
+                  navigate("/login");
+                }}
+                className="mt-4 bg-green-500 text-white font-bold py-2 px-4 rounded"
+              >
+                Login
+              </button>
+              <button
+                onClick={() => setShowNotLoggedInPopup(false)}
+                className="mt-4 bg-red-500 text-white font-bold py-2 px-4 rounded"
+              >
+                Cancel
+              </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
