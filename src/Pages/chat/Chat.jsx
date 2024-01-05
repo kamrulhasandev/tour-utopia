5a371df8077ba388049c2e8e36c7b18336c73d23
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2024-01-06 00:42:46 +0600

    changed

--- src/Pages/chat/Chat.jsx
+++ src/Pages/chat/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = () => {
       </h1>
       <div className="flex justify-center">
         <iframe
-          className="w-[400px] h-screen"
+          className="w-[400px] h-[calc(100vh-200px)]"
           src="https://www.chatbase.co/chatbot-iframe/vu0sTSw3VvY6nAKNpRuAp"
           width="100%"
           frameBorder="0"
